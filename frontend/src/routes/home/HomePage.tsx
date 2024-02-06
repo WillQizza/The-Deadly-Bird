8b4c477cdde74f73a4a5ef03d91f00790bb80e3d
WillQizza <williamqipizza@gmail.com>
2024-02-06 11:49:17 -0700

    homepage to use page component

--- frontend/src/routes/home/HomePage.tsx
+++ frontend/src/routes/home/HomePage.tsx
@@ -1,26 +1,14 @@
-import React, { Fragment } from 'react';
-import LoginForm from '../../components/login/LoginForm';
+import React from 'react';
 
 import styles from "./HomePage.module.css";
-import Footer from '../../components/layout/Footer';
+import Page from '../../components/layout/Page';
 
 const HomePage: React.FC = () => {
     
     return ( 
-        <Fragment>
-            <div className={styles.HomePageContainer}>
-                <div className={styles.imageColumn}>
-                </div>
-                <div className={styles.imageColorColumn}/>
-                <div className={styles.loginColumn}>
-                    <div className={styles.loginTitle}>
-                        The Deadly Bird
-                    </div>
-                    <LoginForm />
-                </div>
-            </div>
-            <Footer />
-        </Fragment>
+        <Page>
+            TODO: Homepage
+        </Page>
     );
 };
 
