dd993d0f4902de224c20d6a71a40625e76ca9e47
WillQizza <williamqipizza@gmail.com>
2024-02-11 03:00:08 -0700

    large chunk of profile work

--- frontend/src/routes/profile/ProfilePage.tsx
+++ frontend/src/routes/profile/ProfilePage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { publicDir } from "../../constants";
+
 import Page from '../../components/layout/Page';
 
 import styles from './ProfilePage.module.css';
@@ -8,20 +10,41 @@ const ProfilePage: React.FC = () => {
     // GET request on user to request actual API?...
     return <Page>
         <div id={styles.container}>
-            <div id={styles.header}>
-                <div id={styles.information}>
-                    <div id={styles.avatarContainer}>
-                        <img src="" />
+            <div id={styles.header} style={{ position: "relative" }}>
+                <div id={styles.avatarContainer}>
+                    <img alt="Profile Avatar" src="https://avatars.githubusercontent.com/u/46386037?s=48&v=4" />
+                </div>
+                <div id={styles.identityContainer}>
+                    <h1 id={styles.username}>Username</h1>
+                    <h5 id={styles.bio}>I don't really know what I'm doing...</h5>
+                </div>
+                <div style={{ flexGrow: 1, display: "flex", justifyContent: "flex-end", gap: 10, marginRight: 10, marginBottom: 10 }}>
+                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                        <div style={{ border: "1px solid black", paddingTop: 2.5, paddingBottom: 2.5, paddingLeft: 30, paddingRight: 30 }}>
+                            Follow
+                        </div>
                     </div>
-                    <div>
-                        <h1 id={styles.username}>Username</h1>
+                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                        <div>
+                            <span style={{ fontSize: "1em" }}>Posts</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                        <div>
+                            <span style={{ fontSize: "1em" }}>Following</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                        <div>
+                            <span style={{ fontSize: "1em" }}>Followers</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                        </div>
                     </div>
                 </div>
-                <div id={styles.stats}>
-
+                <div style={{ position: "absolute", right: 0, top: 0, width: 48, height: 48, margin: 10 }}>
+                    <img alt="Github Account" style={{ height: "100%", width: "100%" }} src={`${publicDir}/static/github.png`} />
                 </div>
             </div>
-            <div>
+            <div id={styles.feed}>
                 {/* TODO: Profile feed */}
             </div>
         </div>
