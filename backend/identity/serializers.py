b2d8837a2668fd9df38f871c27eb4c00addd6c01
WillQizza <williamqipizza@gmail.com>
2024-02-11 04:24:39 -0700

    hook up profile page to backend

--- backend/identity/serializers.py
+++ backend/identity/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from posts.models import Post
+from following.models import Following
 from .models import Author
 
 
@@ -7,10 +9,22 @@ class AuthorSerializer(serializers.ModelSerializer):
   displayName = serializers.CharField(source='user.username')
   profileImage = serializers.CharField(source='profile_picture')
   url = serializers.CharField(source='profile_url')
+  posts = serializers.SerializerMethodField()
+  followers = serializers.SerializerMethodField()
+  following = serializers.SerializerMethodField()
+
+  def get_posts(self, obj: Author):
+    return Post.objects.filter(author=obj).count()
+  
+  def get_followers(self, obj: Author):
+    return Following.objects.filter(target_author=obj).count()
+
+  def get_following(self, obj: Author):
+    return Following.objects.filter(author=obj).count()
 
   class Meta:
     model = Author
-    fields = ['type', 'id', 'url', 'host', 'displayName', 'github', 'profileImage']
+    fields = ['type', 'id', 'url', 'host', 'displayName', 'github', 'profileImage', 'posts', 'followers', 'following']
 
 class AuthorListSerializer(serializers.Serializer):
   """
--- frontend/src/routes/profile/ProfilePage.tsx
+++ frontend/src/routes/profile/ProfilePage.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { publicDir } from "../../constants";
+import { baseURL, publicDir } from "../../constants";
 
 import Page from '../../components/layout/Page';
 
 import styles from './ProfilePage.module.css';
+import { apiRequest } from '../../utils/request';
+import { useParams } from 'react-router-dom';
 
 const ProfilePage: React.FC = () => {
     // GET request on user to request actual API?...
+    const [avatarURL, setAvatarURL] = useState(`${publicDir}/static/default-avatar.png`);
+    const [githubUsername, setGithubUsername] = useState("");
+    const [username, setUsername] = useState("");
+    const [bio, setBio] = useState("I don't really know what I'm doing...");     // TODO: Do we want a bio?
+    const [postCount, setPostCount] = useState(-1);
+    const [followingCount, setFollowingCount] = useState(-1);
+    const [followerCount, setFollowerCount] = useState(-1);
+
+    const params = useParams();
+
+    useEffect(() => {
+        const userId = params["id"];
+        apiRequest(`${baseURL}/api/authors/${userId}/`)
+            .then(async r => {
+                if (r.status !== 200) {
+                    console.error(`Failed to load user profile: ${userId} (status: ${r.status})`);
+                    return;
+                }
+
+                const data = await r.json();
+                setUsername(data["displayName"]);
+                setGithubUsername(data["github"]);
+                setPostCount(data["posts"]);
+                setFollowerCount(data["followers"]);
+                setFollowingCount(data["following"]);
+
+                if (data["profileImage"] !== null) {
+                    setAvatarURL(data["profileImage"]);
+                }
+            });
+    }, [params]);
+
     return <Page>
         <div id={styles.container}>
             <div id={styles.header} style={{ position: "relative" }}>
                 <div id={styles.avatarContainer}>
-                    <img alt="Profile Avatar" src="https://avatars.githubusercontent.com/u/46386037?s=48&v=4" />
+                    <img alt="Profile Avatar" src={avatarURL} />
                 </div>
                 <div id={styles.identityContainer}>
-                    <h1 id={styles.username}>Username</h1>
-                    <h5 id={styles.bio}>I don't really know what I'm doing...</h5>
+                    <h1 id={styles.username}>{username}</h1>
+                    <h5 id={styles.bio}>{bio}</h5>
                 </div>
                 <div id={styles.statsAndFollow}>
                     <div className={styles.item}>
@@ -26,23 +60,27 @@ const ProfilePage: React.FC = () => {
                     </div>
                     <div className={styles.item}>
                         <div>
-                            <span>Posts</span> <span className={styles.itemAmount}>75</span>
+                            <span>Posts</span> <span className={styles.itemAmount}>{postCount === -1 ? "" : postCount}</span>
                         </div>
                     </div>
                     <div className={styles.item}>
                         <div>
-                            <span>Following</span> <span className={styles.itemAmount}>23</span>
+                            <span>Following</span> <span className={styles.itemAmount}>{followingCount === -1 ? "" : followingCount}</span>
                         </div>
                     </div>
                     <div className={styles.item}>
                         <div>
-                            <span>Followers</span> <span className={styles.itemAmount}>73</span>
+                            <span>Followers</span> <span className={styles.itemAmount}>{followerCount === -1 ? "" : followerCount}</span>
                         </div>
                     </div>
                 </div>
-                <div id={styles.githubContainer}>
-                    <img alt="Github Account" style={{ height: "100%", width: "100%" }} src={`${publicDir}/static/github.png`} />
-                </div>
+                {githubUsername ? (
+                    <a href={`https://github.com/${githubUsername}`} target="_blank" rel="noreferrer">
+                        <div id={styles.githubContainer}>
+                            <img alt="Github Account" style={{ height: "100%", width: "100%" }} src={`${publicDir}/static/github.png`} />
+                        </div>
+                    </a>
+                ) : null}
             </div>
             <div id={styles.feed}>
                 {/* TODO: Profile feed */}
