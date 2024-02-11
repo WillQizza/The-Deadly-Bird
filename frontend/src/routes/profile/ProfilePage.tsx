00d96d47299b16f0b9e3d6117a914497f74278a6
WillQizza <williamqipizza@gmail.com>
2024-02-11 03:56:49 -0700

    move styling to .module.css

--- frontend/src/routes/profile/ProfilePage.tsx
+++ frontend/src/routes/profile/ProfilePage.tsx
@@ -18,29 +18,29 @@ const ProfilePage: React.FC = () => {
                     <h1 id={styles.username}>Username</h1>
                     <h5 id={styles.bio}>I don't really know what I'm doing...</h5>
                 </div>
-                <div style={{ flexGrow: 1, display: "flex", justifyContent: "flex-end", gap: 10, marginRight: 10, marginBottom: 10 }}>
-                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
-                        <div style={{ border: "1px solid black", paddingTop: 2.5, paddingBottom: 2.5, paddingLeft: 30, paddingRight: 30 }}>
+                <div id={styles.statsAndFollow}>
+                    <div className={styles.item}>
+                        <div id={styles.followButton}>
                             Follow
                         </div>
                     </div>
-                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                    <div className={styles.item}>
                         <div>
-                            <span style={{ fontSize: "1em" }}>Posts</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                            <span>Posts</span> <span className={styles.itemAmount}>75</span>
                         </div>
                     </div>
-                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                    <div className={styles.item}>
                         <div>
-                            <span style={{ fontSize: "1em" }}>Following</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                            <span>Following</span> <span className={styles.itemAmount}>23</span>
                         </div>
                     </div>
-                    <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column" }}>
+                    <div className={styles.item}>
                         <div>
-                            <span style={{ fontSize: "1em" }}>Followers</span> <span style={{ fontSize: "1.5em" }}>75</span>
+                            <span>Followers</span> <span className={styles.itemAmount}>73</span>
                         </div>
                     </div>
                 </div>
-                <div style={{ position: "absolute", right: 0, top: 0, width: 48, height: 48, margin: 10 }}>
+                <div id={styles.githubContainer}>
                     <img alt="Github Account" style={{ height: "100%", width: "100%" }} src={`${publicDir}/static/github.png`} />
                 </div>
             </div>
