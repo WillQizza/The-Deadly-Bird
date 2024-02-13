7f023dd799d07bc7d8584f48b079465b93081c82
Justin Meimar <meimar@ualberta.ca>
2024-02-13 12:05:09 -0700

    start follow request route

--- backend/following/views.py
+++ backend/following/views.py
@@ -80,9 +80,7 @@ def modify_follower(request, author_id, foreign_author_id):
 @api_view(["POST"])
 def request_follower(request, local_author_id:int, foreign_author_id:int):
     """
-    Note: No URL format specified on the spec. I think this is because the only foreign host interfaces through sending
-    messages in the foreign authors inbox, so this view is like a private view for our server -- no foreign host needs this
-    route. 
+    Request a follower on local or foreign host.
     """
     if request.method == "POST":
 
@@ -98,129 +96,33 @@ def request_follower(request, local_author_id:int, foreign_author_id:int):
         local_author = Author.objects.get(id=local_author_id)
         foreign_author = Author.objects.get(id=foreign_author_id)
         
+        if Following.objects.filter(author__id=local_author_id,
+                                    target_author__id=foreign_author_id).exists(): 
+            return Response({
+                "message": "Conflict: Author is already following"
+            }, status=409)
+
+        elif FollowingRequest.objects.filter(author__id=local_author_id, 
+            target_author__id=foreign_author_id).exists():
+                return Response({
+                    "message": "Conflict: Outstanding request in existence"
+                }, status=409)
+
         if local_author.host != foreign_author.host:
             # TODO: handle remote host inbox object: https://uofa-cmput404.github.io/general/project.html#friendfollow-request
-            pass 
+            return Response({
+                "error": True,
+                "message": "remote inboxed not supported yet"
+            })
         else:
-            pass
-            # request is between local authors
+            FollowingRequest.objects.create(
+                target_author_id = foreign_author_id,
+                local_author_id = local_author_id
+            )
 
+            # TODO: notfiy inbox 
 
-### OLD VIEWS ###
-
-# @api_view(["POST"])
-# @csrf_exempt
-# def follow(request, target_author_id:int): 
-
-#     session_valid = validate_login_session(request)
-#     if isinstance(session_valid, Response):
-#         return session_valid
-
-#     author_id = request.session["id"]
-    
-#     if not check_authors_exist(author_id, target_author_id):
-#         return Response({
-#             "message": "Author does not exist"
-#         }, status=404)
-
-#     if Following.objects.filter(author__id=author_id, target_author__id=target_author_id).exists(): 
-#         return Response({
-#             "message": "Conflict: Author is already following"
-#         }, status=409)
-
-#     if FollowingRequest.objects.filter(author__id=author_id, 
-#         target_author__id=target_author_id).exists():
-#             return Response({
-#                 "message": "Conflict: Outstanding request in existence"
-#             }, status=409)
-
-#     FollowingRequest.objects.create(
-#         author_id=author_id, target_author_id=target_author_id
-#     )
-
-#     return Response({"message": "Created: Follow Request"}, status=201)
-
-# @api_view(["POST"])
-# @csrf_exempt
-# def accept_follow(request, follow_req_id:int):
-    
-#     session_valid = validate_login_session(request)
-#     if isinstance(session_valid, Response):
-#         return session_valid
-    
-#     author_id = request.session["id"]
-#     if not check_authors_exist(author_id):
-#         return Response({
-#             "message": "Author does not exist"
-#         }, status=404) 
-    
-#     if not FollowingRequest.objects.filter(id=follow_req_id).exists(): 
-#             return Response({
-#                 "message": "Not Available: No pending follow request"
-#             }, status=409)
-
-#     try:
-#         follow_request = FollowingRequest.objects.get(id=follow_req_id, target_author_id=author_id)
-#         Following.objects.create(
-#             author_id=follow_request.author_id,
-#             target_author_id=author_id
-#         )
-#         follow_request.delete()
-#         return Response({"message": "Follow Accepted"}, status=201)
-    
-#     except Exception as e:
-#         return Response({"message": f"Internal Server Error: Follow could not be accepted {e}"}, status=500)
-         
-
-# @api_view(["POST"])
-# @csrf_exempt
-# def unfollow(request, target_author_id:int):
- 
-#     session_valid = validate_login_session(request)
-#     if isinstance(session_valid, Response):
-#         return session_valid
-    
-#     author_id = request.session["id"]
-    
-#     if not check_authors_exist(author_id, target_author_id):
-#         return Response({
-#             "message": "Author does not exist"
-#         }, status=404) 
-    
-#     if not Following.objects.filter(author__id=author_id, target_author__id=target_author_id).exists(): 
-#         return Response({
-#             "message": "Not Available: Author is not following"
-#         }, status=409)
-
-#     Following.objects.delete(
-#         author_id=author_id, target_author_id=target_author_id
-#     )
-
-#     return Response({"message": "Created: Follow Request"}, status=201)
-
-# @api_view(["GET"])
-# def get_follow_requests(request, author_id:int):
-    
-#     # get all follow requests where the source author is the query parameter 
-#     following_reqs = FollowingRequest.objects.filter(target_author_id=author_id)
-    
-#     serializer = FollowingRequestSerializer(following_reqs, many=True)
-#     return Response(serializer.data)
-
-# @api_view(["GET"])
-# def get_following(request, author_id:int):
-    
-#     # get all follow requests where the source author is the query parameter 
-#     following = Following.objects.filter(author_id=author_id)
- 
-#     serializer = FollowingSerializer(following)
-#     return Response(serializer.data)
-
-# @api_view(["GET"])
-# def get_followers(request, author_id:int):
-    
-#     # get all follow requests where the source author is the query parameter 
-#     following = Following.objects.filter(target_author_id=author_id)
- 
-#     serializer = FollowingSerializer(following)
-#     return Response(serializer.data)
+            return Response({
+                "error": False,
+                "message": "Follow Request Created"
+            }, status=201)
