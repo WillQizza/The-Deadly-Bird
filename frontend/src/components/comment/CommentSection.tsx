294e0ace4ce5ca348729377c19c3a8f45490c2a5
Irene Sun <93227340+IreneRSun@users.noreply.github.com>
2024-03-18 15:13:38 -0600

    Implemented fetching of comments for the comment section.

--- frontend/src/components/comment/CommentSection.tsx
+++ frontend/src/components/comment/CommentSection.tsx
@@ -1,43 +1,110 @@
 import { useEffect, useRef, useState } from "react";
 import { ListGroup, Image, Col, Row, Alert } from "react-bootstrap";
+import { ReactComponent as Heart } from 'bootstrap-icons/icons/heart.svg';
+import { ReactComponent as HeartFilled } from 'bootstrap-icons/icons/heart-fill.svg';
+import Markdown from 'react-markdown';
 import styles from "./CommentSection.module.css";
 import { baseURL } from "../../constants";
-import { getUserId } from "../../utils/auth";
 import { apiRequest } from "../../utils/request";
 
 interface CommentSectionProps {
-    postId: string
+    postId: string,
+    authorId: string
 }
 
 interface CommentProps {
+    id: string,
     authorUrl: string,
     authorName: string,
     profileImg: string,
     comment: string,
-    date: string
+    contentType: string,
+    date: string,
+    likes: number,
+    isLiked: boolean
 }
 
 const CommentSection: React.FC<CommentSectionProps> = (props: CommentSectionProps) => {
-    const { postId } = props;
-    const [comments, setComments] = useState<CommentProps[]>([{
-        authorUrl: "/",
-        authorName: "Hello World",
-        profileImg: "https://th.bing.com/th/id/R.84a17e40341b39efb41844322681df18?rik=Dnof5ZF8%2fyLpFA&pid=ImgRaw&r=0",
-        comment: "Butterfly Day lorem ipsumijwiopefjoipjowin ejfioajosijd jwiejfpojqskd joijaOInsdjvn jifajoiwjeopfj ijiojiojewoifjijj jijojewfpoijojjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiioooooooooooooooooooooooooooooooooooooooooooooooooppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp",
-        date: "Today" 
-    },
-    {
-        authorUrl: "/",
-        authorName: "New Person",
-        profileImg: "https://zvelo.com/wp-content/uploads/2018/11/anatomy-of-a-full-path-url-hostname-tld-path-protocol.jpg",
-        comment: "So tired T~T",
-        date: "Yesterday" 
-    }]);
+    const { postId, authorId } = props;
+    const [comments, setComments] = useState<CommentProps[]>([]);
     const [responseMessage, setResponseMessage] = useState<string>("");
+    const commentRef = useRef<HTMLAnchorElement>(null);
     const pageSize = 5;
     const currentPage = useRef(1);
 
-    {/** Comment section */}
+    /** Function for fetching more comments */
+    const fetchComments = async () => {
+        // Request comments data
+        const response = await apiRequest(
+            `${baseURL}/api/authors/${authorId}/posts/${postId}/comments/?page=${currentPage.current}&size=${pageSize}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+            }
+        );
+        // Handle response
+        const data = await response.json();
+        if (response.ok) {
+            let newComments: CommentProps[] = [];
+            for (const commentData of data.comments) {
+                newComments.push({
+                    id: commentData.id,
+                    authorUrl: commentData.author.url,
+                    authorName: commentData.author.displayName,
+                    profileImg: commentData.author.profileImage,
+                    comment: commentData.comment,
+                    contentType: commentData.contentType,
+                    date: commentData.published,
+                    likes: 0,
+                    isLiked: false
+                });
+            }
+            setComments([...comments, ...newComments]);
+        } else {
+            setResponseMessage(data.message);
+        }
+    };
+
+    /** Retrieves initial comments on mount */
+    useEffect(() => {
+        fetchComments();
+    }, []);
+
+    /** Retrieves comments while scrolling */
+    useEffect(() => {
+        // check if comments need generated
+        if (Math.floor(comments.length / pageSize) < currentPage.current) {
+            return;
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    currentPage.current++;
+                    fetchComments();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            root: null,
+            threshold: 0,
+        });
+
+        // begin observe
+        if (commentRef.current) {
+            observer.observe(commentRef.current);
+        }
+
+        // cleanup function
+        return () => {
+            if (commentRef.current) {
+                observer.unobserve(commentRef.current);
+            }
+        }
+    }, [comments])
+
+    /** Comment section */
     return (
         <>
             {/** Alert for request errors */}
@@ -50,8 +117,8 @@ const CommentSection: React.FC<CommentSectionProps> = (props: CommentSectionProp
             ) : null}
             {/** Comments section */}
             <ListGroup>
-                {comments.map((comment) => (
-                    <ListGroup.Item>
+                {comments.map((comment, index) => (
+                    <ListGroup.Item key={comment.id} ref={index === comments.length - 1 ? commentRef : null}>
                         <Row>
                             <Col className={styles.commentImageContainer}>
                                 <Image src={comment.profileImg} roundedCircle width={50} height={50} />
@@ -59,7 +126,11 @@ const CommentSection: React.FC<CommentSectionProps> = (props: CommentSectionProp
                             <Col className={styles.commentInfoContainer}>
                                 <a href={comment.authorUrl} className={styles.author}>{comment.authorName}</a>
                                 <p className={styles.date}>{comment.date}</p>
-                                <p className={styles.comment}>{comment.comment}</p>
+                                {comment.contentType == "text/markdown"? (
+                                    <Markdown className={styles.comment}>{comment.comment}</Markdown>
+                                ) : (
+                                    <p className={styles.comment}>{comment.comment}</p>
+                                )}
                             </Col>
                         </Row>
                     </ListGroup.Item>
