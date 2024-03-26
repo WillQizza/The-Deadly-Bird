79bc4fc56d47dc98791e5325c6bc57fec25f604d
Irene Sun <93227340+IreneRSun@users.noreply.github.com>
2024-03-26 12:50:56 -0600

    Use setting host instead of environment host.

--- backend/nodes/views.py
+++ backend/nodes/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpRequest
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from deadlybird.permissions import SessionAuthenticated, RemoteOrSessionAuthenticated
-import os
+from deadlybird.permissions import RemoteOrSessionAuthenticated
+from deadlybird.settings import SITE_HOST_URL
 
 @api_view(["GET"])
 @permission_classes([RemoteOrSessionAuthenticated])
@@ -11,5 +11,4 @@ def _getHostname(request: HttpRequest):
   private method used by frontend to get the hostname for distinguishing between local
   and remote authors
   """
-  hostname = os.environ.get("HOST_URL")
-  return Response({"hostname": hostname}, status=200) 
+  return Response({"hostname": SITE_HOST_URL}, status=200) 
