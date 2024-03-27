89d7ffd3b7b9315680342cdc6867eb7f98d26249
WillQizza <williamqipizza@gmail.com>
2024-03-27 04:18:31 -0600

    fix PUT method failure

--- backend/following/urls.py
+++ backend/following/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
           view=views.following, name="following"),
 
      re_path(r'^authors/(?P<author_id>[\w-]+)/followers/(?P<foreign_author_id>.*)/$', views.modify_follower, name="modify_follower"),
+     re_path(r'^authors/(?P<author_id>[\w-]+)/followers/(?P<foreign_author_id>.*)$', views.modify_follower, name="modify_follower2"),
     
      path("authors/request-follower/<str:author_id>/<str:target_author_id>",
           views.request_follower, name="request_follower")
