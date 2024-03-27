2eb865c962619cc4283907fcbc559271620b8605
WillQizza <williamqipizza@gmail.com>
2024-03-27 04:08:52 -0600

    support dashes too

--- backend/following/urls.py
+++ backend/following/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
      path("authors/<str:author_id>/following",
           view=views.following, name="following"),
 
-     re_path(r'^authors/(?P<author_id>\w+)/followers/(?P<foreign_author_id>.*)/$', views.modify_follower, name="modify_follower"),
+     re_path(r'^authors/(?P<author_id>[\w-]+)/followers/(?P<foreign_author_id>.*)/$', views.modify_follower, name="modify_follower"),
     
      path("authors/request-follower/<str:author_id>/<str:target_author_id>",
           views.request_follower, name="request_follower")
